b6b1944bcdb3e2ae0f060885d4322b532b45c357
byedcg <byedcg@gmail.com>
2022-03-15 21:13:16 +0100

    basic application + docker image

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fastapi-docker-starter
 FastAPI boilerplate for easy development and deployment.
+
+## Development
+
+### Run the server
+
+```shell
+uvicorn app.main:app --reload
+```
+
+### Addresses
+
+```shell
+localhost:8000/
+localhost:8000/docs
+localhost:8000/redoc
+```
---
+++ app/main.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from fastapi import FastAPI, Request, Response
+from pydantic import BaseModel
+import time
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# middleware run on every request
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response: Response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.get("/echo/{message}")
+async def echo(message: str, query: Optional[str] = None):
+    time.sleep(1)
+    return {"message": message, "query": query}
+
+
+# data model
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+@app.post("/send")
+def send_data(item: Item):
+    item_dict = item.dict()
+    return item_dict
